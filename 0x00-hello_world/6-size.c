da1dce728221dba203470c7f8c4e3564263c2fb6
Willowusu <williamowusu1998@gmail.com>
2023-02-16 20:13:13 -0800

    Added a file to print size of various datatypes in c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ int main(void)
 	long long int longLongIntType;
 	float floatType;
 	/* sizeof returns size of type */
-	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
-	printf("Size of an int: %ld byte(s)\n", sizeof(intType));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(longIntType));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(longLongIntType));
-	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %lu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %lu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(longIntType));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(longLongIntType));
+	printf("Size of a float: %lu byte(s)\n", sizeof(floatType));
 
 	return (0);
 }
